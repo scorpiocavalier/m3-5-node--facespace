dafac2d3011863cc3e187a5be8195dc8a656504e
Arturo Chen <art.can.fly@gmail.com>
2020-07-08 15:39:18 -0400

    3 done

--- server.js
+++ server.js
@@ -18,13 +18,26 @@ const handleHomepage = (req, res) => {
 
 const handleProfilePage = (req, res) => {
   const { id } = req.params
-  const user = users.filter(user => user._id === id)[0]
+  const user = users.find(user => user._id === id)
   let friends = []
   user.friends.forEach(friendId => {
-    let friend = users.filter(user => friendId === user._id)[0]
+    let friend = users.find(user => friendId === user._id)
     friends.push(friend)
   })
   res.render('pages/profile', { user, friends })
+}
+
+const handleSignin = (req, res) => {
+  res.render('pages/signin')
+}
+
+const handleName = (req, res) => {
+  const { firstName } = req.body
+  const user = users.find(user => user.name === firstName)
+  if(user !== undefined)
+    res.status(200).redirect(`/users/${user._id}`)
+  else
+    res.status(404).render('pages/signin')
 }
 
 // --------------------------------------------------------------
@@ -34,7 +47,11 @@ express()
   .use(express.urlencoded({ extended: false }))
   .set('view engine', 'ejs')
 
+  .post('/getname', handleName)
+
   .get('/users/:id', handleProfilePage)
+
+  .get('/signin', handleSignin)
 
   .get('/', handleHomepage)
 
