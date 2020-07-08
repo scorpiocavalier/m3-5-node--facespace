19382115f838a6037f210235209db72f33e21df7
Arturo Chen <art.can.fly@gmail.com>
2020-07-08 14:55:05 -0400

    2 done

--- server.js
+++ server.js
@@ -16,12 +16,25 @@ const handleHomepage = (req, res) => {
   res.status(200).render('pages/homepage', { users })
 }
 
+const handleProfilePage = (req, res) => {
+  const { id } = req.params
+  const user = users.filter(user => user._id === id)[0]
+  let friends = []
+  user.friends.forEach(friendId => {
+    let friend = users.filter(user => friendId === user._id)[0]
+    friends.push(friend)
+  })
+  res.render('pages/profile', { user, friends })
+}
+
 // --------------------------------------------------------------
 express()
   .use(morgan('dev'))
   .use(express.static('public'))
   .use(express.urlencoded({ extended: false }))
   .set('view engine', 'ejs')
+
+  .get('/users/:id', handleProfilePage)
 
   .get('/', handleHomepage)
 
