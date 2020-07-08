f2c366f83ac533971f1d3733697cbccfedb229be
Arturo Chen <art.can.fly@gmail.com>
2020-07-08 11:02:38 -0400

    homepage endpoint

--- server.js
+++ server.js
@@ -12,17 +12,18 @@ const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
-// -----------------------------------------------------
-// server endpoints
+// --------------------------------------------------------------
 express()
   .use(morgan('dev'))
   .use(express.static('public'))
   .use(express.urlencoded({ extended: false }))
   .set('view engine', 'ejs')
 
-  // endpoints
+  .get('/', (req, res) => {
+    res.render('pages/homepage')
+  })
 
-  // a catchall endpoint that will send the 404 message.
   .get('*', handleFourOhFour)
 
   .listen(8000, () => console.log('Listening on port 8000'));
+// --------------------------------------------------------------
