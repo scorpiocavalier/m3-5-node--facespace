70d983b6182a390a98b221227a9da1fe3a7ee8d7
Arturo Chen <art.can.fly@gmail.com>
2020-07-08 21:15:40 -0400

    created handlers.js

---
+++ public/js/handlers.js
@@ -0,0 +1,35 @@
+const handleFourOhFour = (req, res) => {
+  res.status(404).send("I couldn't find what you're looking for.")
+}
+
+const handleHomepage = (req, res) => {
+  res.status(200).render('pages/homepage', { users, currentUser })
+}
+
+const handleProfilePage = (req, res) => {
+  const { id } = req.params
+  const profileUser = users.find(user => user._id === id)
+  let friends = users.filter(user => profileUser.friends.includes(friend._id))
+  res.status(200).render('pages/profile', { profileUser, friends, currentUser })
+}
+
+const handleSignin = (req, res) => {
+  if(Object.entries(currentUser).length === 0)
+    res.status(200).render('pages/signin', { currentUser })
+  else
+    res.status(404).redirect('/')
+}
+
+const handleName = (req, res) => {
+  const { firstName } = req.body
+  const user = users.find(user => user.name.toLowerCase() === firstName.toLowerCase())
+  if(user !== undefined) {
+    currentUser = user
+    res.status(200).redirect(`/users/${user._id}`)
+  } else
+    res.status(404).render('pages/signin', { currentUser })
+}
+
+module.exports = { 
+  handleFourOhFour, handleHomepage, handleProfilePage, handleSignin, handleName 
+}
--- server.js
+++ server.js
@@ -4,48 +4,14 @@ const express = require('express')
 const morgan = require('morgan')
 
 const { users } = require('./data/users')
+const { 
+  handleName, handleProfilePage,
+  handleSignin, handleHomepage,
+  handleFourOhFour
+} = require('./public/js/handlers')
 
 let currentUser = {}
 
-// declare the 404 function
-const handleFourOhFour = (req, res) => {
-  res.status(404).send("I couldn't find what you're looking for.")
-}
-
-const handleHomepage = (req, res) => {
-  console.log(currentUser.friends)
-  res.status(200).render('pages/homepage', { users, currentUser })
-}
-
-const handleProfilePage = (req, res) => {
-  const { id } = req.params
-  const user = users.find(user => user._id === id)
-  let friends = []
-  user.friends.forEach(friendId => {
-    let friend = users.find(user => friendId === user._id)
-    friends.push(friend)
-  })
-  res.status(200).render('pages/profile', { user, friends, currentUser })
-}
-
-const handleSignin = (req, res) => {
-  if(Object.entries(currentUser).length === 0)
-    res.status(200).render('pages/signin', { currentUser })
-  else
-    res.status(404).redirect('/')
-}
-
-const handleName = (req, res) => {
-  const { firstName } = req.body
-  const user = users.find(user => user.name === firstName)
-  if(user !== undefined) {
-    currentUser = user
-    res.status(200).redirect(`/users/${user._id}`)
-  } else
-    res.status(404).render('pages/signin', { currentUser })
-}
-
-// --------------------------------------------------------------
 express()
   .use(morgan('dev'))
   .use(express.static('public'))
@@ -63,4 +29,3 @@ express()
   .get('*', handleFourOhFour)
 
   .listen(8000, () => console.log('Listening on port 8000'))
-// --------------------------------------------------------------
